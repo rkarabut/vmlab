f6aed4164caca5779d148ad551663932f982f336
Ratmir Karabut <rkarabut@gmail.com>
2020-06-16 06:47:07 +0300

    Silence progress bars, fix looknfeel dconf load

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,16 +4,16 @@
 sudo groupadd -r autologin
 sudo useradd -m -p saA3Gu85b.CZM ri -G sudo,autologin -s /bin/bash # password: ri
 
-sudo apt-get -q -y update
+sudo apt-get -qq -y update
 
 # select the best apt source
-sudo apt-get -q -y install aptitude curl netselect-apt whois
+sudo apt-get -qq -y install aptitude curl netselect-apt whois
 
-COUNTRY=$(whois $(curl ifconfig.me) | grep country: | awk '{print $2}')
+COUNTRY=$(whois $(curl -s ifconfig.me) | grep country: | awk '{print $2}')
 sudo netselect-apt -a amd64 -c $COUNTRY -s -n -o /etc/apt/sources.list testing >/dev/null 2>&1
 
 # update to testing
-sudo apt-get -q -y update
+sudo apt-get -qq -y update
 
 # aptitude autofixes some held packages problems after switching to testing
 sudo aptitude -q -y install cmake build-essential
@@ -23,5 +23,5 @@ sudo sed -i 's/.*autologin-user.*/autologin-user=ri/' /etc/lightdm/lightdm.conf
 sudo sed -i 's/.*autologin-session.*/autologin-session=mate/' /etc/lightdm/lightdm.conf 
 
 for d in $(find /tmp/scripts -mindepth 1 -maxdepth 1 -type d); do
-  ( cd $d && ./setup.sh )
+  ( cd $d && bash ./setup.sh )
 done
--- scripts/looknfeel/setup.sh
+++ scripts/looknfeel/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt-get install dconf-cli greybird-gtk-theme tango-icon-theme numix-gtk-theme
-sudo -u ri dconf load / < ./dconf-settings	
+sudo -u ri dbus-launch dconf load / < ./dconf-settings	
