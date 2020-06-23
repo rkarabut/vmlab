0a9eb2cefeb04bd2cdf34b58ef7c90ec81a104c9
Ratmir Karabut <rkarabut@gmail.com>
2020-06-23 15:34:10 +0300

    Add Ghidra build scripts

--- scripts/user/ghidra_build/setup.sh
+++ scripts/user/ghidra_build/setup.sh
@@ -3,6 +3,8 @@ set -e
 
 DIR=$(pwd)
 
+cd 
+
 sudo apt-get -y install openjdk-11-jre
 
 curl -s "https://get.sdkman.io" | bash
@@ -18,18 +20,17 @@ gradle --init-script gradle/support/fetchDependencies.gradle init
 gradle buildGhidra
 )
 
-cd
 mkdir -p work/ && cd work
 
 GHIDRA_DIR=$(unzip -t $HOME/ghidra/build/dist/ghidra*.zip | head -n2 | tail -n1 | awk '{print $2}')
-unzip $DIR/ghidra/build/dist/ghidra*.zip
-rm -rf $DIR/ghidra
-ln -s $GHIDRA_DIR/ghidraRun $HOME/ghidra
-mkdir -p $HOME/.ghidra/$GHIDRA_DIR
+unzip -qq -n $HOME/ghidra/build/dist/ghidra*.zip
+rm -rf $HOME/ghidra
+ln -s $HOME/work/$GHIDRA_DIR/ghidraRun $HOME/ghidra
+mkdir -p $HOME/.ghidra/.$GHIDRA_DIR
 
-echo "USER_AGREEMENT=ACCEPT" >> $HOME/.ghidra/$GHIDRA_DIR/preferences
-echo "SHOW_TIPS=false" >> $HOME/.ghidra/$GHIDRA_DIR/preferences
-echo "GhidraShowWhatsNew=false" >> $HOME/.ghidra/$GHIDRA_DIR/preferences
+echo "USER_AGREEMENT=ACCEPT" >> $HOME/.ghidra/.$GHIDRA_DIR/preferences
+echo "SHOW_TIPS=false" >> $HOME/.ghidra/.$GHIDRA_DIR/preferences
+echo "GhidraShowWhatsNew=false" >> $HOME/.ghidra/.$GHIDRA_DIR/preferences
 
 
 
