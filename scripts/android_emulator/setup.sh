d2a06df5ecf7c3f4f4ad41d217c314354b06b376
Ratmir Karabut <rkarabut@gmail.com>
2020-06-23 06:39:07 +0300

    Fix Android emulator setup

--- scripts/android_emulator/setup.sh
+++ scripts/android_emulator/setup.sh
@@ -9,19 +9,19 @@ cd
 SDK_URL=`curl -s https://developer.android.com/studio\#downloads | grep -Po "(https://).+?(commandlinetools-linux-\d+).+?latest.zip"`
 
 # download the tools
-wget -q -O "android-sdk-tools.zip" $SDK_URL
-unzip android-sdk-tools.zip && rm -f ./android-sdk-tools.zip
+wget -q -c -O "android-sdk-tools.zip" $SDK_URL
+unzip -qq -n android-sdk-tools.zip && rm -f ./android-sdk-tools.zip
 
-mkdir -p $HOME/.android/sdk
-mv ./tools $HOME/.android/sdk
+mkdir -p .android/sdk
+mv tools .android/sdk
 
-(cd $HOME/.android/sdk
+(cd .android/sdk
 
 AVD_NAME=5.1_WVGA_API_25
 
 export JAVA_HOME=~/android-studio/jre/
-yes | ./tools/bin/sdkmanager --sdk_root=$HOME/.android/sdk --install "platforms;android-25" "emulator" "platform-tools" > /dev/null
-yes | ./tools/bin/sdkmanager --sdk_root=$HOME/.android/sdk --install "system-images;android-25;default;x86" > /dev/null
+yes | ./tools/bin/sdkmanager --sdk_root=~/.android/sdk --install "platforms;android-25" "emulator" "platform-tools" > /dev/null
+yes | ./tools/bin/sdkmanager --sdk_root=~/.android/sdk --install "system-images;android-25;default;x86" > /dev/null
 ./tools/bin/avdmanager create avd -d "5.1in WVGA" -n "$AVD_NAME" -k "system-images;android-25;default;x86" --force > /dev/null
 
 echo 'hw.cpu.ncore=1' >> ~/.android/avd/$AVD_NAME.avd/config.ini
