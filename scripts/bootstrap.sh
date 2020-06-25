32ca6c423d2c2af9dd2dd81857f465eaba6ee67f
Ratmir Karabut <rkarabut@gmail.com>
2020-06-25 13:48:35 +0300

    Add jq

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -31,7 +31,8 @@ sudo apt-get -qq -y update
 
 # aptitude autofixes some held packages problems after switching to testing
 sudo aptitude -q=2 -y install cmake build-essential
-sudo aptitude -q=2 -y install lightdm mate-desktop-environment vim-nox fish git firefox-esr python3-pip
+sudo aptitude -q=2 -y install lightdm mate-desktop-environment vim-nox fish git \
+    jq firefox-esr python3-pip
 
 sudo sed -i "s/.*autologin-user.*/autologin-user = $VM_USERNAME/" /etc/lightdm/lightdm.conf 
 sudo sed -i 's/.*autologin-session.*/autologin-session = mate/' /etc/lightdm/lightdm.conf 
