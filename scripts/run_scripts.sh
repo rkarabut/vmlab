04fc54cac148f3213fc7313c27de7a75c4a88689
Ratmir Karabut <rkarabut@gmail.com>
2020-06-26 05:05:10 +0300

    Fix, update pentest_tools script

--- scripts/run_scripts.sh
+++ scripts/run_scripts.sh
@@ -2,7 +2,7 @@
 
 for d in $(find /tmp/scripts -mindepth 1 -maxdepth 1 -type d | sort); do
   echo "Running $(basename $d) ..."
-  ( cd $d && chown -R $VM_USERNAME * && su $VM_USERNAME setup.sh )
+  ( cd $d && chown -R $VM_USERNAME $d && su $VM_USERNAME setup.sh )
   rm -rf $d
   echo "Finished running $(basename $d)"
 done
