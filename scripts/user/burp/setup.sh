ea1bb64182bbe26a12cfb707ec7114ac81880b38
Ratmir Karabut <rkarabut@gmail.com>
2020-06-26 04:46:54 +0300

    Fix burpsuite_communtiy script

--- scripts/user/burp/setup.sh
+++ scripts/user/burp/setup.sh
@@ -3,12 +3,12 @@ set -e
 
 sudo apt-get -qq -y install openjdk-11-jre
 
-cd /tmp
-BURP_FILENAME=$(curl -sL https://portswigger.net/burp/releases/community/latest | grep burpsuite_community.*jar | sed 's/.*\(burpsuite_community.*\.jar\).*/\1/')
-wget -c -q https://portswigger.net/burp/releases/download/$BURP_FILENAME -O burpsuite_community.jar
-
 mkdir -p $HOME/work/burp
-echo '#!/bin/bash\njava -jar ./burpsuite_community.jar' >> $HOME/work/burp/burpsuite_community.sh
+
+BURP_FILENAME=$(curl -sL https://portswigger.net/burp/releases/community/latest | grep burpsuite_community.*jar | sed 's/.*\(burpsuite_community.*\.jar\).*/\1/')
+wget -c -q https://portswigger.net/burp/releases/download/$BURP_FILENAME -O $HOME/work/burp/burpsuite_community.jar
+
+echo "#!/bin/bash\njava -jar $HOME/work/burp/burpsuite_community.jar" >> $HOME/work/burp/burpsuite_community.sh
 chmod +x $HOME/work/burp/burpsuite_community.sh
 
 mkdir -p $HOME/.local/bin
