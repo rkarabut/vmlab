2d303e2e04507a0ae48bb30dcd73f894afbeff7f
Ratmir Karabut <rkarabut@gmail.com>
2020-07-09 21:10:58 +0300

    Fix ranger script (config)

--- scripts/user/ranger/setup.sh
+++ scripts/user/ranger/setup.sh
@@ -5,8 +5,8 @@ sudo apt-get -qq -y install libimage-exiftool-perl graphicsmagick-imagemagick-co
 
 sudo pip install ranger-fm -q
 ranger --copy-config=all
-sed -i 's/^set preview_images.*$/set preview_images true/' $HOME/.config/ranger/rc.conf
-sed -i 's/^set preview_images_method.*$/set preview_images_method terminology/' $HOME/.config/ranger/rc.conf
+sed -i 's/^set preview_images .*$/set preview_images true/' $HOME/.config/ranger/rc.conf
+sed -i 's/^set preview_images_method .*$/set preview_images_method terminology/' $HOME/.config/ranger/rc.conf
 
 mkdir -p $HOME/.config/terminology/config/standard/
 cp terminology/base.cfg $HOME/.config/terminology/config/standard/
