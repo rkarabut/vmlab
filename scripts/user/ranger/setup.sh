128f560b052f826a76d5f8a09f0e369c55730d54
Ratmir Karabut <rkarabut@gmail.com>
2020-07-12 04:31:20 +0300

    Add terminology launcher when needed

--- scripts/user/ranger/setup.sh
+++ scripts/user/ranger/setup.sh
@@ -3,7 +3,7 @@ set -e
 
 sudo apt-get -qq -y install libimage-exiftool-perl graphicsmagick-imagemagick-compat terminology
 
-sudo pip install ranger-fm -q
+sudo pip3 install ranger-fm -q
 ranger --copy-config=all
 sed -i 's/^set preview_images .*$/set preview_images true/' $HOME/.config/ranger/rc.conf
 sed -i 's/^set preview_images_method .*$/set preview_images_method terminology/' $HOME/.config/ranger/rc.conf
@@ -11,3 +11,7 @@ sed -i 's/^set preview_images_method .*$/set preview_images_method terminology/'
 mkdir -p $HOME/.config/terminology/config/standard/
 cp terminology/base.cfg $HOME/.config/terminology/config/standard/
 
+if [[ ! -z $(which add_mate_launcher.sh) ]]; then
+    # TODO: dconf refuses to work correctly without sudo -u $(whoami) 
+    sudo -u $(whoami) add_mate_launcher.sh /usr/share/applications/terminology.desktop
+fi
