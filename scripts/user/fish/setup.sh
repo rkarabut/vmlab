d808f5c83daeba96fd447ec948ceeff5e11197f3
Ratmir Karabut <rkarabut@gmail.com>
2020-07-18 21:50:43 +0300

    Build fish from master

--- scripts/user/fish/setup.sh
+++ scripts/user/fish/setup.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -e
 
-sudo apt-get -qq -y install fish
+# apt-get completion is broken in fish 3.1.2, building from master
+# sudo apt-get -qq -y install fish
+
+sudo apt-get -qq -y install libncurses-dev cmake
+
+git clone https://github.com/fish-shell/fish-shell.git
+cd fish
+mkdir build && cd build
+cmake ..
+make && sudo make install
+sudo ln -s /usr/local/bin/fish /usr/bin/fish
 
 if [[ ! -d $HOME/.local/share/omf ]]; then 
     curl -s -L https://get.oh-my.fish > omf.fish
