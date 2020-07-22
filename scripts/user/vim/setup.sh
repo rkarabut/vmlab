df58e50063a6a396dd647c3c410161fa663c2345
Ratmir Karabut <rkarabut@gmail.com>
2020-07-22 15:36:44 +0300

    Fix indent

--- scripts/user/vim/setup.sh
+++ scripts/user/vim/setup.sh
@@ -6,7 +6,7 @@ sudo apt-get -qq -y install vim-nox fzf silversearcher-ag libpython3-dev xdg-uti
 cp .vimrc ~/.vimrc
 if [[ ! -d ~/.vim/autoload/plug.vim ]] ; then
     curl -sfLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 vim -E -s -c "source ~/.vimrc" -c PlugInstall -c qa
