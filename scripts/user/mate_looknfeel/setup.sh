ff0462171f2b3ac1fbd526baab878a30439740db
Ratmir Karabut <rkarabut@gmail.com>
2021-04-01 09:23:37 +0000

    Add Parcellite

--- scripts/user/mate_looknfeel/setup.sh
+++ scripts/user/mate_looknfeel/setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -e
 
-sudo apt-get -qq -y install dconf-cli greybird-gtk-theme tango-icon-theme numix-gtk-theme
+sudo apt-get -qq -y install dconf-cli greybird-gtk-theme tango-icon-theme numix-gtk-theme \
+    parcellite
+
 XDG_RUNTIME_DIR="/run/user/$(id -u)" dbus-launch dconf load / < ./dconf-settings
+
+mkdir -p $HOME/.config/parcellite
+cp parcelliterc $HOME/.config/parcellite
 
 chmod +x add_mate_launcher.sh
 sudo cp add_mate_launcher.sh /usr/local/bin/
